cd130792fe112b87a7c638c0be49f684f9849aa6
pizarrosch <bayenrshc89@gmail.com>
2024-04-12 21:08:31 +0200

    First Commit - create input container with buttons

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,39 @@
+import {TextInput, View, StyleSheet} from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function StartGameScreen() {
+  return (
+    <View style={styles.mainContainer}>
+      <TextInput style={styles.input} maxLength={2}/>
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: '#8c2c5b',
+    elevation: 4, //for Android only
+    shadowColor: '#8C2C5BFF',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    shadowOpacity: 1,
+    alignItems: 'center'
+  },
+  input: {
+    height: 50,
+    width: 50,
+    fontSize: 34,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
