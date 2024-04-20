5b15c50e509cfd787e984f1655174c7ad1e2eb97
pizarrosch <bayenrshc89@gmail.com>
2024-04-20 18:44:37 +0200

    Add the summary text below the image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import {View, Image, StyleSheet} from "react-native";
+import {View, Image, StyleSheet, Text} from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/Colors";
 
@@ -9,6 +9,9 @@ export default function GameOverScreen() {
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={require('../assets/images/success.png')} />
       </View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlightText}>X</Text> attempts to guess your number <Text style={styles.highlightText}>Y</Text>
+      </Text>
     </View>
   )
 }
@@ -32,5 +35,14 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%'
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize: 23,
+    textAlign: 'center'
+  },
+  highlightText: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.violet4
   }
 })
